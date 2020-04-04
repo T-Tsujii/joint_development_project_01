67d6eb8975b8267b89ff1b1c971e0dc94b486f93
KawazuKento <myv@outlook.jp>
2020-04-04 22:23:17 +0900

    一覧表示画面の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
 class TasksController < ApplicationController
-    def index
-      @tasks = Task.all
+  def index
+    @tasks = Task.all
   end
 
-    def new
-      @task = Task.new
+  def new
+    @task = Task.new
   end
 
-    def create
-      Task.create(task_params)
+  def create
+    Task.create(task_params)
   end
 
-    private
-    
-    def task_params
-      params.require(:task).permit(:title, :content)
-    end
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
