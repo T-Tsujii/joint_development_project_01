6542ccc3723061f22d487979733d335c363c3471
udagawayuuki mac <udagawayuukimac@udagawayuukinoMacBook-Pro.local>
2020-04-05 11:45:05 +0900

    新規投稿機能のcreate.html.erbの追加をしました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,17 @@
 class MessagesController < ApplicationController
+
+  # 新規投稿画面に対応するアクション
   def new
     @message = Message.new
   end
 
+  # 新規登録画面に対応するアクション
   def create
     Message.create(message_params)
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
 end
